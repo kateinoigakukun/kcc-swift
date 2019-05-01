0753f20baa92167bb83533c11b76a19ef5dc8c78
Yuta Saito <kateinoigakukun@gmail.com>
2019-05-02 00:03:23 +0900

    Add example

---
+++ examples/hello.c
@@ -0,0 +1,14 @@
+int main() {
+    print_char(72);
+    print_char(101);
+    print_char(108);
+    print_char(108);
+    print_char(111);
+    print_char(32);
+    print_char(87);
+    print_char(111);
+    print_char(114);
+    print_char(108);
+    print_char(100);
+    print_char(33);
+}
