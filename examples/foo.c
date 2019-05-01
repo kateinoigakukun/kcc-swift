0f59042de97c4a6fd05bd538a527da2fa6b846db
Yuta Saito <kateinoigakukun@gmail.com>
2019-05-02 00:21:54 +0900

    Update foo

--- examples/foo.c
+++ examples/foo.c
@@ -1,8 +1,10 @@
 void foo(int value1, int value2) {
-    print_char(value2);
-    print_char(value1);
-    print_char(value1);
-    print_char(value1);
+    print_char(48);
+    bar(value2);
+}
+void bar(int baz) {
+    print_char(49);
+    foo(baz, 21);
 }
 int main() {
     foo(61, 65);
