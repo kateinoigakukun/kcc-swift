f2bbc55f0bc1d2ee2bbc0a749ddeb2ef42c1c611
Yuta Saito <kateinoigakukun@gmail.com>
2019-05-08 18:05:33 +0900

    Update abcd.c

--- examples/abcd.c
+++ examples/abcd.c
@@ -2,10 +2,8 @@ int rec(int count) {
     print_char(count);
     if(count-100) {
         rec(count+1);
-        return;
-    } else {
-        return;
     }
+    return;
 }
 void main() {
     rec(97);
